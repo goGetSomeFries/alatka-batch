c85bd905a7ef2bd85cc6ec3432c4e65e9c3a884d
mocker <liuyibing123456@126.com>
2026-04-21 15:20:56 +0800

    emaple模块示例优化

---
+++ alatka-batch-example/src/main/java/com/alatka/batch/example/listener/DefaultJobExecutionListener1.java
@@ -0,0 +1,23 @@
+package com.alatka.batch.example.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultJobExecutionListener1 implements JobExecutionListener {
+
+    private Logger logger = LoggerFactory.getLogger(DefaultJobExecutionListener1.class);
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        this.logger.info("Job execution started");
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        this.logger.info("Job execution ended");
+    }
+}
