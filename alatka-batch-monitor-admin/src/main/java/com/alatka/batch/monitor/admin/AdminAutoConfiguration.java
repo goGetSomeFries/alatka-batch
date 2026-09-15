3f842fee9737e7134b669527626da497ca902a34
mocker <liuyibing123456@126.com>
2026-09-15 18:12:11 +0800

    init

---
+++ alatka-batch-monitor-admin/src/main/java/com/alatka/batch/monitor/admin/AdminAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.alatka.batch.monitor.admin;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = {"com.alatka.batch.monitor.admin", "com.alatka.batch.infra"})
+@Configuration
+public class AdminAutoConfiguration {
+
+}
